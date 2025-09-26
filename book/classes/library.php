afa277eeff4cae949a931244abf8ec266c10c2b7
Amae-star <jessamaeferrer021706@gmail.com>
2025-09-26 16:54:12 +0800

    Add files via upload

---
+++ book/classes/library.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once "database.php";
+class Book extends Database {
+    public $id="";
+    public $title="";
+    public $author="";
+    public $genre="";
+    public $publication_year="";
+    
+
+    public function addBook(){
+        $sql = "INSERT INTO book(title, author,genre,pub_year) VALUE(:title, :author, :genre, :publication_year)";
+        $query = $this->connect()->prepare($sql);
+        $query->bindParam(":title", $this->title);
+        $query->bindParam(":author", $this->author);
+        $query->bindParam(":genre", $this->genre);
+        $query->bindParam(":publication_year", $this->publication_year);
+
+        return $query->execute();
+    }
+
+     public function viewBook($search="", $genre_filter=""){
+     if (!empty($search) && !empty($genre_filter)) {
+        $sql = "SELECT * FROM book WHERE title LIKE CONCAT('%', :search, '%') AND genre = :genre ORDER BY title ASC";
+        $query = $this->connect()->prepare($sql);
+        $query->bindParam(":search", $search);
+        $query->bindParam(":genre", $genre_filter);
+
+        } elseif (!empty($search)) {
+        $sql = "SELECT * FROM book WHERE title LIKE CONCAT('%', :search, '%') ORDER BY title ASC";
+        $query = $this->connect()->prepare($sql);
+        $query->bindParam(":search", $search);
+
+        } elseif (!empty($genre_filter)) {
+        $sql = "SELECT * FROM book WHERE genre = :genre ORDER BY title ASC";
+        $query = $this->connect()->prepare($sql);
+        $query->bindParam(":genre", $genre_filter);
+
+        } else {
+        $sql = "SELECT * FROM book ORDER BY title ASC";
+        $query = $this->connect()->prepare($sql);
+        }
+
+        if ($query->execute()) {
+        return $query->fetchAll();
+        } else {
+        return null;
+        }
+    }
+
+    public function isBookExist($booktitle){
+        $sql = "SELECT COUNT(*) as total FROM book WHERE title = :title";
+        $query= $this->connect()->prepare($sql);
+        $query->bindParam(":title", $booktitle);
+        $record = null;
+        if($query->execute()){
+            $record = $query->fetch();
+        }
+
+        if($record["total"] > 0){
+            return true;
+        }else{
+            return false;
+        }
+        
+
+    }
+}
+
+// $obj = new Book();
+// $obj->title = "Cinderella";
+// $obj->author = "Ferrer";
+// $obj->genre = "Fiction";
+// $obj->publication_year=1990's;
+
+// $obj->addBook();
